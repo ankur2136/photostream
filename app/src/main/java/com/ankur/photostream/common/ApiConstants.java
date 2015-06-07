1514757a4e5b3ea38a67bf6022d568df7d4f4470
Ankur Jain <ankur@bsb.in>
2015-06-07 21:52:21 +0530

    Added access token

--- app/src/main/java/com/ankur/photostream/common/ApiConstants.java
+++ app/src/main/java/com/ankur/photostream/common/ApiConstants.java
@@ -1,5 +1,7 @@
 package com.ankur.photostream.common;
 
+import com.facebook.AccessToken;
+
 public class ApiConstants {
 
     public interface Photo {
@@ -17,6 +19,8 @@ public class ApiConstants {
     }
 
     private static String getPhotoUrl(int page, int pageSize) {
-        return "https://graph.facebook.com/v2.3/me/photos?fields=id,from,images&limit=2&access_token=CAAMYCMaGJZA0BAPMnqTqEQy2GsavxqlNUUci5xoDAqVMWMs5cLQbHPUrirHg88sXHMoudURLkiF5LKHmDkFpNk8mVMWGMKxt8tjkONLuqEcxHvnvu8PV0IMj6oz5qb1SamLRDrVMLcSLh3Bes5GmSZAsW6asdAkiYfig6kS71SfAy9cg5zsZBfuleYuAR7E5YZCLr67ATglbLTaQ57tU";
+        AccessToken.getCurrentAccessToken().getToken();
+        return "https://graph.facebook.com/v2.3/me/photos?fields=id,from,images&limit=2&access_token="
+                + AccessToken.getCurrentAccessToken().getToken();
     }
 }
